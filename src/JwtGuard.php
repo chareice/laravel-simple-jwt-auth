d9dfd76a8b06393f0bd51f8eb085a69a60963984
Chareice <chareice@live.com>
2021-09-16 08:49:07 +0800

    Fix: test

--- src/JwtGuard.php
+++ src/JwtGuard.php
@@ -10,101 +10,102 @@ use Illuminate\Http\Request;
 
 class JwtGuard implements Guard
 {
-  use GuardHelpers;
+    use GuardHelpers;
 
-  protected JWTService $JWTService;
-  protected string $inputKey;
-  protected Request $request;
+    protected JWTService $JWTService;
+    protected string $inputKey;
+    protected Request $request;
 
-  public function __construct(
-    JWTService $JWTService,
-    Request $request,
-    UserProvider $provider,
-    string $inputKey = 'token'
-  )
-  {
-    $this->request = $request;
-    $this->provider = $provider;
-    $this->inputKey = $inputKey;
-    $this->JWTService = $JWTService;
-  }
-
-  /**
-   * Get the currently authenticated user.
-   *
-   * @return \Illuminate\Contracts\Auth\Authenticatable|null
-   */
-  public function user(): ?\Illuminate\Contracts\Auth\Authenticatable
-  {
-    if (!is_null($this->user)) {
-      return $this->user;
+    public function __construct(
+        JWTService $JWTService,
+        Request $request,
+        UserProvider $provider,
+        string $inputKey = 'token'
+    )
+    {
+        $this->request = $request;
+        $this->provider = $provider;
+        $this->inputKey = $inputKey;
+        $this->JWTService = $JWTService;
     }
 
-    $token = $this->getTokenForRequest();
+    /**
+     * Get the currently authenticated user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function user(): ?\Illuminate\Contracts\Auth\Authenticatable
+    {
+        if (!is_null($this->user)) {
+            return $this->user;
+        }
 
-    if (is_null($token)) {
-      return null;
+        $token = $this->getTokenForRequest();
+
+        if (is_null($token)) {
+            return null;
+        }
+
+        $payload = $this->getSubFromToken($token);
+
+        return $this->user = $this->provider->retrieveById($payload[$this->JWTService->getSubKey()]);
     }
 
-    $payload = $this->getSubFromToken($token);
-
-    return $this->user = $this->provider->retrieveById($payload[$this->JWTService->getSubKey()]);
-  }
-
-  protected function getSubFromToken(string $token)
-  {
-    return $this->JWTService->tokenPayload($token);
-  }
-
-  public function getTokenForRequest()
-  {
-    $request = request();
-    $token = $request->query($this->inputKey);
-
-    if (empty($token)) {
-      $token = $request->input($this->inputKey);
+    protected function getSubFromToken(string $token)
+    {
+        return $this->JWTService->tokenPayload($token);
     }
 
-    if (empty($token)) {
-      $token = $request->bearerToken();
+    public function getTokenForRequest()
+    {
+        $request = request();
+        $token = $request->query($this->inputKey);
+
+        if (empty($token)) {
+            $token = $request->input($this->inputKey);
+        }
+
+        if (empty($token)) {
+            $token = $request->bearerToken();
+        }
+
+        if (empty($token)) {
+            $token = $request->getPassword();
+        }
+
+        return $token;
     }
 
-    if (empty($token)) {
-      $token = $request->getPassword();
+    public function validate(array $credentials = [])
+    {
+        $authenticatable = $this->provider->retrieveByCredentials($credentials);
+
+        if (is_null($authenticatable)) {
+            return false;
+        }
+
+        return $this->provider->validateCredentials($authenticatable, $credentials);
     }
 
-    return $token;
-  }
-
-  public function validate(array $credentials = [])
-  {
-    $authenticatable = $this->provider->retrieveByCredentials($credentials);
-
-    if (is_null($authenticatable)) {
-      return false;
+    public function login(JWTSubject $subject): string
+    {
+        $token = $this->JWTService->tokenFromSubject($subject);
+        $this->setUser($subject);
+        return $token;
     }
 
-    return $this->provider->validateCredentials($authenticatable, $credentials);
-  }
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+        // 新的Request 置空User
+        $this->user = null;
+        return $this;
+    }
 
-  public function login(JWTSubject $subject): string
-  {
-    $token = $this->JWTService->tokenFromSubject($subject);
-    $this->setUser($subject);
-    return $token;
-  }
+    public function setProvider(UserProvider $provider)
+    {
+        $this->provider = $provider;
 
-  public function setRequest(Request $request)
-  {
-    $this->request = $request;
-
-    return $this;
-  }
-
-  public function setProvider(UserProvider $provider)
-  {
-    $this->provider = $provider;
-
-    return $this;
-  }
+        return $this;
+    }
 }
--- tests/JwtServiceTest.php
+++ tests/JwtServiceTest.php
@@ -1,30 +1,34 @@
 <?php
+
 class TestSubject implements \Chareice\SimpleJwtAuth\Contracts\JWTSubject
 {
-  private int $id;
-  public function __construct(int $id)
-  {
-    $this->id = $id;
-  }
+    use \Illuminate\Auth\Authenticatable;
 
-  public function getJWTIdentifier(): int
-  {
-    return $this->id;
-  }
+    private int $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getJWTIdentifier(): int
+    {
+        return $this->id;
+    }
 }
 
 it("encode from JWTSubject", function () {
-  $id = 100;
-  $subject = new TestSubject($id);
-  $JwtSecret = "test";
+    $id = 100;
+    $subject = new TestSubject($id);
+    $JwtSecret = "test";
 
-  $service = new \Chareice\SimpleJwtAuth\JWTService($JwtSecret);
+    $service = new \Chareice\SimpleJwtAuth\JWTService($JwtSecret, 'id');
 
-  $token = $service->tokenFromSubject($subject);
+    $token = $service->tokenFromSubject($subject);
 
-  $this->assertTrue(is_string($token));
+    $this->assertTrue(is_string($token));
 
-  $payload = $service->tokenPayload($token);
-  $this->assertTrue(array_key_exists('id', $payload));
-  $this->assertTrue($id === $payload['id']);
+    $payload = $service->tokenPayload($token);
+    $this->assertTrue(array_key_exists('id', $payload));
+    $this->assertTrue($id === $payload['id']);
 });
